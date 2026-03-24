3865203c2909abc67707c7c37dde2f1f1334f872
RadinateProject <ketan@radinate.com>
2026-03-24 21:45:18 +0530

    sorted sets

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,10 +1,23 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey,usernamesUniqueKey } from '$services/keys';
+import { usersKey,usernamesUniqueKey, usernamesKey } from '$services/keys';
 
 
-export const getUserByUsername = async (username: string) => {};
+export const getUserByUsername = async (username: string) => {
+  //Use the username argument to look persons userID with username sorted sets
+  const decimalId = await client.zScore(usernamesKey(), username)
+  //make sure we actually got an id and lookup
+  if(!decimalId){
+    throw new Error('User does not exist')
+  }
+  //take id and convert to hex
+  const id = decimalId.toString(16)
+  //use id to look up user's hash
+  const user = await client.hGetAll(usersKey(id))
+  //deserialize and return the hash
+  return deserialize(id,user)
+};
 
 export const getUserById = async (id: string) => {
   const user = await client.hGetAll(usersKey(id))
@@ -22,6 +35,10 @@ export const createUser = async (attrs: CreateUserAttrs) => {
 
   await client.hSet(usersKey(id), serialize(attrs))
   await client.sAdd(usernamesUniqueKey(), attrs.username)
+  await client.zAdd(usernamesKey(),{
+    value: attrs.username,
+    score: parseInt(id,16)
+  })
 
   return id
 };
