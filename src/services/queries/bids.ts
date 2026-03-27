26dafb07d5c23d5b2a21b3bfc625033126120d9c
RadinateProject <ketan@radinate.com>
2026-03-27 10:20:25 +0530

    locks and more

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,15 +1,22 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { bidHistoryKey, itemsKey, itemsByPriceKey } from '$services/keys';
-import { client } from '$services/redis';
+import { client, withLock } from '$services/redis';
 import { DateTime } from 'luxon';
 import { getItem } from './items';
 
-export const createBid = async (attrs: CreateBidAttrs) => {
-	// transaction in redis
-	return client.executeIsolated(async (isolatedClient) => {
-		await isolatedClient.watch(itemsKey(attrs.itemId)) 
+const pause = (duration: number) => {
+	return new Promise((resolve) => {
+		setTimeout(resolve, duration);
+	});
+};
 
+export const createBid = async (attrs: CreateBidAttrs) => {
+	return withLock(attrs.itemId, async (lockedClient: typeof client, signal: any) => {
+		// 1) Fetching the item
+		// 2) Doing validation
+		// 3) Writing some data
 		const item = await getItem(attrs.itemId);
+
 		if (!item) {
 			throw new Error('Item does not exist');
 		}
@@ -22,27 +29,64 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 
 		const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
 
-		return isolatedClient.multi()
-		.rPush(bidHistoryKey(attrs.itemId), serialized)
-		.hSet(itemsKey(item.id), {
+		if (signal.expired) {
+			throw new Error('Lock expired, cant write any more data');
+		}
+
+		return Promise.all([
+			lockedClient.rPush(bidHistoryKey(attrs.itemId), serialized),
+			lockedClient.hSet(itemsKey(item.id), {
 				bids: item.bids + 1,
 				price: attrs.amount,
 				highestBidUserId: attrs.userId
+			}),
+			lockedClient.zAdd(itemsByPriceKey(), {
+				value: item.id,
+				score: attrs.amount
 			})
-			.zAdd(itemsByPriceKey(),{
-				value:item.id,
-				score:attrs.amount
-			})
-			.exec()
-	
+		]);
 	});
+
+	// vs. transaction based approach (gives error on edge cases)
+	// return client.executeIsolated(async (isolatedClient) => {
+	// 	await isolatedClient.watch(itemsKey(attrs.itemId));
+
+	// 	const item = await getItem(attrs.itemId);
+
+	// 	if (!item) {
+	// 		throw new Error('Item does not exist');
+	// 	}
+	// 	if (item.price >= attrs.amount) {
+	// 		throw new Error('Bid too low');
+	// 	}
+	// 	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+	// 		throw new Error('Item closed to bidding');
+	// 	}
+
+	// 	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+
+	// 	return isolatedClient
+	// 		.multi()
+	// 		.rPush(bidHistoryKey(attrs.itemId), serialized)
+	// 		.hSet(itemsKey(item.id), {
+	// 			bids: item.bids + 1,
+	// 			price: attrs.amount,
+	// 			highestBidUserId: attrs.userId
+	// 		})
+	// 		.zAdd(itemsByPriceKey(), {
+	// 			value: item.id,
+	// 			score: attrs.amount
+	// 		})
+	// 		.exec();
+	// });
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
-	// LRANGE history#a1 (-1*offset - count) (-1 - offset)
 	const startIndex = -1 * offset - count;
 	const endIndex = -1 - offset;
+
 	const range = await client.lRange(bidHistoryKey(itemId), startIndex, endIndex);
+
 	return range.map((bid) => deserializeHistory(bid));
 };
 
@@ -52,6 +96,7 @@ const serializeHistory = (amount: number, createdAt: number) => {
 
 const deserializeHistory = (stored: string) => {
 	const [amount, createdAt] = stored.split(':');
+
 	return {
 		amount: parseFloat(amount),
 		createdAt: DateTime.fromMillis(parseInt(createdAt))
